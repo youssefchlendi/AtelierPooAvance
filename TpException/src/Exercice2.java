03ff97da9c39db9dd42f611528fc0aee9b9fd737
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-10 00:07:44 +0100

    add catch numberFormatException

--- TpException/src/Exercice2.java
+++ TpException/src/Exercice2.java
@@ -3,9 +3,13 @@ public class Exercice2 {
     public static void main(String[] args) {
         int i = 0;
         int resu = 0;
+        try {
             for (i = 0; i < args.length; i++) {
                 resu = resu + Integer.parseInt(args[i]);
             }
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage() + " A la position " + i);
+        }
         System.out.println("Resultat " + resu);
         System.out.println("Fin du programme");
 
