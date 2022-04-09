cd2650ef94ed147148df79a1d5abf0bc289352b5
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-10 00:00:58 +0100

    catch InputMismatchException on read

--- TpException/src/exceptionTest.java
+++ TpException/src/exceptionTest.java
@@ -1,19 +1,25 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class exceptionTest {
     public static void main(String[] args) {
         int a, b, res;
         Scanner clavier = new Scanner(System.in);
+        try {
         System.out.println("a =");
         a = clavier.nextInt();
         System.out.println("b =");
         b = clavier.nextInt();
-        try {
             res = a / b;
             System.out.println(a + " / " + b + " = " + res);
         } catch (ArithmeticException e) {
             System.out.println("oops ! un problème dans la division");
             System.out.println("le message officiel est " + e.getMessage());
+        } catch (InputMismatchException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("le bloc finally sera toujours exécuté");
+            System.out.println("c'est là qu'on fermera par exemple les fichiers");
         }
         clavier.close();
         System.out.println("Fin du programme");
