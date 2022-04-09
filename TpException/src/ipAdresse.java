84eebd978a3e695d96424e530908e5685cdaecf2
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-10 00:14:32 +0100

    add throwing exception and try/catch block

--- TpException/src/ipAdresse.java
+++ TpException/src/ipAdresse.java
@@ -1,7 +1,32 @@
 public class ipAdresse {
     private int[] octet = { 0, 0, 0, 0 };
 
-    public ipAdresse(int a, int b, int c, int d) {
+    public ipAdresse(int a, int b, int c, int d) throws ipAdrException {
+        if (a > 255) {
+            throw new ipAdrException(1, 0);
+        }
+        if (a < 0) {
+            throw new ipAdrException(0, 0);
+        }
+        if (b > 255) {
+            throw new ipAdrException(1, 1);
+        }
+        if (b < 0) {
+            throw new ipAdrException(0, 1);
+        }
+        if (c > 255) {
+            throw new ipAdrException(1, 2);
+        }
+        if (c < 0) {
+            throw new ipAdrException(0, 2);
+        }
+        if (d > 255) {
+            throw new ipAdrException(1, 3);
+        }
+        if (d < 0) {
+            throw new ipAdrException(0, 3);
+        }
+
         octet[0] = a;
         octet[1] = b;
         octet[2] = c;
@@ -13,11 +38,15 @@ public class ipAdresse {
     }
 
     public static void main(String[] args) {
-		ipAdresse ip=null;
-			ip = new ipAdresse(-5,1,1,1);
-		if (ip!=null) {			
-			System.out.print(ip.toString());
-		}
-	}
+        ipAdresse ip = null;
+        try {
+            ip = new ipAdresse(-5, 1, 1, 1);
+        } catch (ipAdrException e) {
+            System.out.println(e.getMessage());
+        }
+        if (ip != null) {
+            System.out.print(ip.toString());
+        }
+    }
 
 }
